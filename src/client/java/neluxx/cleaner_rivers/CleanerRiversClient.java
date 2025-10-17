5968cd82ea40c468ca670bfc6894547f11ded596
Neluxx <fabian.arndt96@proton.me>
2025-10-17 14:25:54 +0200

    Migrate datapack to fabric mod

---
+++ src/client/java/neluxx/cleaner_rivers/CleanerRiversClient.java
@@ -0,0 +1,10 @@
+package neluxx.cleaner_rivers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CleanerRiversClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
