5e06f22e5db8de3f43fab2de6afd39db02682bbb
Neluxx <fabian.arndt96@proton.me>
2026-04-11 20:48:12 +0200

    refactor: cleanup beet plugin

--- src/plugins/disks.py
+++ src/plugins/disks.py
@@ -9,17 +9,13 @@ FEATURES = [
 
 
 def beet_default(ctx: Context):
-    base_version = ctx.meta["base_version"]
-    overlay_versions = ctx.meta["overlay_versions"]
-    vanilla = ctx.inject(Vanilla)
-
     for feature in FEATURES:
-        source = get_source(vanilla, base_version)
+        source = get_source(ctx.inject(Vanilla), ctx.meta["base_version"])
         apply_patch(ctx.data, source, feature, nested_radius=True)
 
-    for directory, version in overlay_versions.items():
+    for directory, version in ctx.meta["overlay_versions"].items():
         overlay = ctx.data.overlays[directory]
-        source = get_source(vanilla, version)
+        source = get_source(ctx.inject(Vanilla), version)
         for feature in FEATURES:
             apply_patch(overlay, source, feature, nested_radius=False)
 
@@ -30,9 +26,12 @@ def get_source(vanilla: Vanilla, version: str):
 
 def apply_patch(pack: DataPack, source, feature: str, nested_radius: bool):
     patched = source[feature].copy()
+
     config = patched.data["config"]
     config["half_height"] = 0
+
     radius = config["radius"]["value"] if nested_radius else config["radius"]
     radius["min_inclusive"] = 0
     radius["max_inclusive"] = 0
+
     pack[WorldgenConfiguredFeature][feature] = patched
